f0b1d84b3adceba3b9fc199a0b4ac76a52098132
ag-beyrem <beyrem.agrebi@proservices-training-company.com>
2025-04-23 14:44:02 +0100

    fix url

--- lib/core/config/env.dart
+++ lib/core/config/env.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:studiffy/utils/app/session/session_manager.dart';
 
-String baseUrl = dotenv.get("BASE_URL");
+String baseUrl = kDebugMode ? dotenv.get("LOCAL_URL") : dotenv.get("PROD_URL");
 String fileUrl =
     '$baseUrl/enterprise-${SessionManager.user.enterprise}/storage';
 
